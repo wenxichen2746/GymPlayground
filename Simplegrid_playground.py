1293d65c353045507f988b836ee6619a6754139e
Wenxichen2746 <wc2746@columbia.edu>
2024-03-10 17:39:29 -0400

    looptrain

--- Simplegrid_playground.py
+++ Simplegrid_playground.py
@@ -76,20 +76,20 @@ maps.append(obstacle_map)
 
 
 obstacle_map = [
+        "0000001",
+        "0000001",
+        "0000001",
+        "0000001",
+        "0000001",
         "0000000",
-        "0000001",
-        "0000001",
-        "0000001",
-        "0000001",
-        "0000001",
-        "1111110",
+        "1111100",
     ]
 maps.append(obstacle_map)
 
 
 env = gym.make(
     'SimpleGrid-v0', 
-    obstacle_map=maps[0], 
+    obstacle_map=maps[3], 
     render_mode='human'
 )
 
@@ -98,24 +98,19 @@ obs, info = env.reset(options={'start_loc':0, 'goal_loc':48})
 print(obs)
 done = env.unwrapped.done
 
-class SymbolicObsWrapper(gym.ObservationWrapper):
-    def __init__(self, env: gym.Env, obstacle_map):
-        gym.ObservationWrapper.__init__(self, env)
-        self.observation_space = 1
-
-    def observation(self, observation):
-        pass
-        
 
 print(env.get_wrapper_attr('goal_xy'))
 
 env=SymbolicGridObsWrapper(env,obstacle_map=maps[0])
 obs, info = env.reset(options={'start_loc':0, 'goal_loc':48})
-print('wrap',obs)
+print('wrapped env obs',obs)
 
-for _ in range(10):
-    if done:
-        break
-    action = env.action_space.sample()
-    obs, reward, done, _, info = env.step(action)
-env.close()
+
+from stable_baselines3 import PPO
+
+model = PPO("CnnPolicy", env, verbose=1,normalize_images=False)
+# Train the model
+model.learn(total_timesteps=10000)
+
+# Save the model
+model.save("GymPlayground/trained_model")
